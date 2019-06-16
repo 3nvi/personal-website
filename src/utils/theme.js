53ebd32429b71f8a67b105a1486cf32968b8861f
3nvi <aggelos@orfium.com>
2019-06-16 15:04:02 +0300

    create theme

---
+++ src/utils/theme.js
@@ -0,0 +1,17 @@
+export default {
+  spacing: {
+    xs: '0.5rem',
+    sm: '1rem',
+    md: '2rem',
+    lg: '3rem',
+    xl: '4rem',
+    xxl: '5rem',
+  },
+  colors: {
+    primary: '#6a61c7',
+    accent: '#CC26E8',
+    black: '#28313B',
+    white: '#fff',
+    grey: '#9B9B9B',
+  },
+};
