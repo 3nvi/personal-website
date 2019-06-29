34f4b261ae8d31a4dfef3ba4a889285a11bea20b
3nvi <aggelos@orfium.com>
2019-06-29 21:19:14 +0300

    add `size` prop to Heading

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,21 +1,32 @@
+import PropTypes from 'prop-types';
 import styled from '@emotion/styled';
 
 const Heading = styled.h1`
   font-family: 'Playfair Display', 'FuturaNew', sans-serif;
-  font-size: 4rem;
-  word-spacing: 100vw;
 
-  @media only screen and (min-width: 768px) {
-    font-size: 6.5rem;
+  @media only screen and (max-width: 767px) {
+    font-size: 3.25rem;
   }
 
-  @media only screen and (min-width: 1200px) {
-    font-size: 8rem;
+  @media only screen and (min-width: 768px) and (max-width: 1199px) {
+    font-size: ${props => (props.size === 'small' ? '4rem' : '6.5rem')};
+  }
+
+  @media only screen and (min-width: 1200px) and (max-width: 1599px) {
+    font-size: ${props => (props.size === 'small' ? '4rem' : '8rem')};
   }
 
   @media only screen and (min-width: 1600px) {
-    font-size: 10rem;
+    font-size: ${props => (props.size === 'small' ? '5rem' : '10rem')};
   }
 `;
+
+Heading.propTypes = {
+  size: PropTypes.oneOf(['small', 'large']),
+};
+
+Heading.defaultProps = {
+  size: 'large',
+};
 
 export default Heading;
