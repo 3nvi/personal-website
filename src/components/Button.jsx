1c31d9d190734f7b5055cd16d31ea927c4bd92c8
3nvi <aggelos@orfium.com>
2019-06-29 21:23:54 +0300

    remove unused props from Button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -7,32 +7,34 @@ const Button = styled.button`
   border: none;
   text-transform: uppercase;
   font-weight: bold;
-  letter-spacing: 0.2rem;
+  letter-spacing: 0.15rem;
   padding: ${props => `${props.theme.spacing.sm} ${props.theme.spacing.lg}`};
-  border-radius: ${props => (props.shape === 'pill' ? '25px' : '4px')};
+  border-radius: 25px;
   background-image: ${props =>
-    props.variation === 'primary'
-      ? `linear-gradient(-45deg, ${props.theme.colors.primary}, ${props.theme.colors.accent})`
-      : undefined};
-  color: ${props =>
-    props.variation === 'primary' ? props.theme.colors.white : props.theme.colors.black};
+    `linear-gradient(-45deg, ${props.theme.colors.primary}, ${props.theme.colors.accent})`};
+  color: ${props => props.theme.colors.white};
   width: ${props => (props.fullWidth ? '100%' : 'unset')};
 
   &:hover {
     transform: scale(1.065, 1.065);
   }
+
+  &[disabled] {
+    opacity: 0.3;
+    pointer-events: none;
+  }
+
+  @media only screen and (max-width: 767px) {
+    width: 100%;
+  }
 `;
 
 Button.propTypes = {
-  variation: PropTypes.oneOf(['primary']),
-  shape: PropTypes.oneOf(['pill', 'rect']),
   fullWidth: PropTypes.bool,
 };
 
 Button.defaultProps = {
   fullWidth: false,
-  variation: 'primary',
-  shape: 'pill',
 };
 
 export default Button;
