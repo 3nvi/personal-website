4924867dbeadedf0a5be0ca4dc554ab86e6a1a21
3nvi <aggelos@orfium.com>
2019-06-29 21:22:57 +0300

    create 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,14 +1,53 @@
 import React from 'react';
-
+import styled from '@emotion/styled';
+import { css } from '@emotion/core';
+import { graphql } from 'gatsby';
+import Img from 'gatsby-image';
 import Layout from '../components/Layout';
+import Heading from '../components/Heading';
 import SEO from '../components/SEO';
+import Container from '../components/Container';
 
-const NotFoundPage = () => (
+const mobileAndTabletHeadingStyle = css`
+  @media only screen and (max-width: 1199px) {
+    border-bottom: 0.5rem solid;
+    border-top: 0.5rem solid;
+    padding: 2rem 0;
+  }
+`;
+
+const ResponsiveImgWrapper = styled.div`
+  flex-shrink: 0;
+
+  @media only screen and (max-width: 1199px) {
+    display: none;
+  }
+`;
+
+const Page404 = props => (
   <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <SEO title="Not Found" />
+    <Container contentDirection="row" contentJustification="space-evenly">
+      <Heading css={mobileAndTabletHeadingStyle}>
+        404 <br /> Not found
+      </Heading>
+      <ResponsiveImgWrapper>
+        <Img fixed={props.data.heroImage.childImageSharp.fixed} alt="Duotone close-up Portrait" />
+      </ResponsiveImgWrapper>
+    </Container>
   </Layout>
 );
 
-export default NotFoundPage;
+export default Page404;
+
+export const query = graphql`
+  query {
+    heroImage: file(relativeDirectory: { eq: "404" }, name: { eq: "hero" }) {
+      childImageSharp {
+        fixed(quality: 85, width: 400, height: 400, toFormat: JPG) {
+          ...GatsbyImageSharpFixed_withWebp_noBase64
+        }
+      }
+    }
+  }
+`;
