2eac0a5e4d06cec9089f27384d64ab86cd1d9f9e
3nvi <aggelos@orfium.com>
2019-06-30 03:28:58 +0300

    add preconnect to gfonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,12 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
+      resolve: 'gatsby-plugin-preconnect',
+      options: {
+        domains: ['https://fonts.gstatic.com'],
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `data`,
