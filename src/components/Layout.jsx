c2cd4457ef2385577841c19028d5eadc403d291a
3nvi <aggelos@orfium.com>
2019-06-30 21:53:00 +0300

    delete .css file and add its content via emotion's `Global` tag

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,15 +1,188 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from '@emotion/styled';
+import { Global, css } from '@emotion/core';
 import { ThemeProvider } from 'emotion-theming';
 import theme from '../utils/theme';
 import NavHeader from './NavHeader';
-import './layout.css';
 import { twitterHandle, githubHandle, linkedinHandle, mediumHandle } from '../utils/constants';
 import TwitterSVG from '../assets/icons/twitter.svg';
 import MediumSVG from '../assets/icons/medium.svg';
 import GithubSVG from '../assets/icons/github.svg';
 import LinkedinSVG from '../assets/icons/linkedin.svg';
+
+const globalStyles = css`
+  @font-face {
+    font-family: 'FuturaNew';
+    font-style: normal;
+    font-weight: 400;
+    font-display: swap;
+    src: url('../assets/fonts/Futura-Book.woff') format('woff');
+  }
+
+  @font-face {
+    font-family: 'FuturaNew';
+    font-style: normal;
+    font-weight: 700;
+    font-display: swap;
+    src: url('../assets/fonts/Futura-Bold.woff') format('woff');
+  }
+
+  @font-face {
+    font-family: 'Playfair Display';
+    font-style: normal;
+    font-weight: 700;
+    font-display: swap;
+    src: local('Playfair Display Bold'), local('PlayfairDisplay-Bold'),
+      url(https://fonts.gstatic.com/s/playfairdisplay/v14/nuFlD-vYSZviVYUb_rj3ij__anPXBYf9lW4e5j5hNKc.woff2)
+        format('woff2');
+    unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC,
+      U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+  }
+
+  html {
+    font-size: 16px;
+    font-family: 'FuturaNew', sans-serif;
+    -ms-text-size-adjust: 100%;
+    -webkit-text-size-adjust: 100%;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  html,
+  body,
+  div,
+  span,
+  applet,
+  object,
+  iframe,
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  p,
+  blockquote,
+  pre,
+  a,
+  abbr,
+  acronym,
+  address,
+  big,
+  cite,
+  code,
+  del,
+  dfn,
+  em,
+  img,
+  ins,
+  kbd,
+  q,
+  s,
+  samp,
+  small,
+  strike,
+  strong,
+  sub,
+  sup,
+  tt,
+  var,
+  b,
+  u,
+  i,
+  center,
+  dl,
+  dt,
+  dd,
+  ol,
+  ul,
+  li,
+  fieldset,
+  form,
+  label,
+  legend,
+  table,
+  caption,
+  tbody,
+  tfoot,
+  thead,
+  tr,
+  th,
+  td,
+  article,
+  aside,
+  canvas,
+  details,
+  embed,
+  figure,
+  figcaption,
+  footer,
+  header,
+  hgroup,
+  menu,
+  nav,
+  output,
+  ruby,
+  section,
+  summary,
+  time,
+  mark,
+  audio,
+  video {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    font-size: 100%;
+    vertical-align: baseline;
+  }
+
+  /* HTML5 display-role reset for older browsers */
+  article,
+  aside,
+  details,
+  figcaption,
+  figure,
+  footer,
+  header,
+  hgroup,
+  menu,
+  nav,
+  section {
+    display: block;
+  }
+
+  body {
+    line-height: 1;
+  }
+
+  ol,
+  ul {
+    list-style: none;
+  }
+
+  blockquote,
+  q {
+    quotes: none;
+  }
+
+  blockquote:before,
+  blockquote:after,
+  q:before,
+  q:after {
+    content: '';
+    content: none;
+  }
+
+  table {
+    border-collapse: collapse;
+    border-spacing: 0;
+  }
+
+  a {
+    text-decoration: none;
+  }
+`;
 
 const ResponsiveWrapper = styled.div`
   position: relative;
@@ -115,6 +288,7 @@ const Copyright = styled.p`
 
 const Layout = ({ children }) => (
   <ThemeProvider theme={theme}>
+    <Global styles={globalStyles} />
     <ResponsiveWrapper>
       <NavHeader />
       <Main>{children}</Main>
