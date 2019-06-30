bc2498049fe0f38fbc1dae30c5b9e3d0e74dd59a
3nvi <aggelos@orfium.com>
2019-06-30 03:29:24 +0300

    move folders

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
     title: 'Aggelos Arvanitakis',
     description:
       'Front end engineer aiming to continuously expand his knowledge by collaborating, learning & teaching. I know & teach React, Svelte, Redux, Apollo, Webperf',
-    image: `static/main.jpg`,
+    image: `/img/main.jpg`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -41,7 +41,7 @@ module.exports = {
         background_color: `#887CFE`,
         theme_color: `#CC26E8`,
         display: `minimal-ui`,
-        icon: `static/favicon.png`, // This path is relative to the root of the site.
+        icon: `static/img/favicon.png`,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
