ef4eb43ca36e84c004b0791c6fbb64374032b0a2
Aggelos Arvanitakis <agg.arvanitakis@gmail.com>
2019-11-27 12:39:05 +0200

    explicitly set `anonymous` value to crossorigin attr

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -2,8 +2,20 @@ const React = require('react');
 
 export const onRenderBody = ({ setHeadComponents }) => {
   setHeadComponents([
-    <link href="/fonts/Futura-Book.woff2" as="font" type="font/woff2" rel="preload" crossOrigin />,
-    <link href="/fonts/Futura-Bold.woff2" as="font" type="font/woff2" rel="preload" crossOrigin />,
+    <link
+      href="/fonts/Futura-Book.woff2"
+      as="font"
+      type="font/woff2"
+      rel="preload"
+      crossOrigin="anonymous"
+    />,
+    <link
+      href="/fonts/Futura-Bold.woff2"
+      as="font"
+      type="font/woff2"
+      rel="preload"
+      crossOrigin="anonymous"
+    />,
     <link
       href="https://fonts.googleapis.com/css?family=Playfair+Display:700&display=swap"
       rel="stylesheet"
