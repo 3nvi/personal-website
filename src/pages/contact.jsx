063c6cdddcaca815d0a149cf24dd5b123059fac2
3nvi <agg.arvanitakis@gmail.com>
2025-03-19 19:55:16 +0200

    refactor: update ContactPage to use Tailwind CSS for styling and simplify form structure

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import axios from 'axios';
-import { css } from '@emotion/core';
-import styled from '@emotion/styled';
 import AutosizeableTextarea from 'react-textarea-autosize';
 import { Form as FormikForm, Formik, Field, ErrorMessage } from 'formik';
 import * as yup from 'yup';
@@ -14,61 +12,23 @@ import Grid from '../components/Grid';
 import Column from '../components/Column';
 import Container from '../components/Container';
 
-const commonFormElementStyles = ({ theme }) => css`
-  border-top: none;
-  border-left: none;
-  border-right: none;
-  padding: 0.5rem 0;
-  width: 100%;
-  font-size: 1rem;
-  font-family: inherit;
-  border-radius: 0;
-  border-bottom: 1px solid ${theme.colors.lightgrey};
-  outline: none;
-  color: ${theme.colors.black};
+const FormError = ({ children }) => <p className="text-xs mt-1 text-red-600">{children}</p>;
 
-  &:focus {
-    border-bottom: 1px solid ${theme.colors.grey};
-  }
-`;
+const Input = ({ form, field, ...rest }) => (
+  <input
+    {...field}
+    {...rest}
+    className="w-full border-0 border-b border-gray-200 py-2 font-inherit rounded-none outline-none text-black focus:border-gray-400"
+  />
+);
 
-const Form = styled(FormikForm)`
-  padding: ${({ theme }) => `${theme.spacing.lg} 0 ${theme.spacing.sm} 0`};
-`;
-
-const FormControl = styled.div`
-  margin-bottom: ${({ theme }) => theme.spacing.lg};
-`;
-
-const Label = styled.label`
-  font-size: 0.9rem;
-  color: ${({ theme }) => theme.colors.grey};
-`;
-
-const Input = styled(({ form, field, ...rest }) => <input {...field} {...rest} />)`
-  ${commonFormElementStyles}
-`;
-
-const Textarea = styled(({ form, field, ...rest }) => (
-  <AutosizeableTextarea {...field} {...rest} />
-))`
-  ${commonFormElementStyles}
-`;
-
-const FormError = styled.p`
-  font-size: 0.8rem;
-  margin-top: ${({ theme }) => theme.spacing.xs};
-  color: ${({ theme }) => theme.colors.danger};
-`;
-
-const SuccessBox = styled.div`
-  padding: ${({ theme }) => `${theme.spacing.md} ${theme.spacing.lg}`};
-  margin-top: ${({ theme }) => theme.spacing.lg};
-  border-radius: 4px;
-  background-color: ${({ theme }) => theme.colors.black};
-  color: ${({ theme }) => theme.colors.white};
-  text-align: center;
-`;
+const Textarea = ({ form, field, ...rest }) => (
+  <AutosizeableTextarea
+    {...field}
+    {...rest}
+    className="w-full border-0 border-b border-gray-200 py-2 font-inherit rounded-none outline-none text-black focus:border-gray-400"
+  />
+);
 
 const initialValues = {
   email: '',
@@ -106,51 +66,57 @@ const ContactPage = () => {
     <Layout>
       <SEO title="Contact" />
 
-      <Container contentJustification="center">
-        <Grid>
-          <Column centered tablet={12} computer={8} largeMonitor={6}>
-            <Heading size="small">Get in touch</Heading>
-            <Text>
-              If you want to get in touch with me after all you've seen, then please fill this form
-              and I'll get back to you within a few hours. Alternatively, feel free to email me
-              directly at{' '}
-              <a href="mailto:contact@aggelosarvanitakis.me">contact@aggelosarvanitakis.me</a>.
-            </Text>
-            {isMessageSent ? (
-              <SuccessBox>Your message has been sent!</SuccessBox>
-            ) : (
-              <Formik
-                initialValues={initialValues}
-                onSubmit={sendMessage}
-                validationSchema={validationSchema}
-              >
-                {({ isSubmitting, isValid }) => (
-                  <Form noValidate>
-                    <FormControl>
-                      <Label htmlFor="email">Your email address</Label>
-                      <Field component={Input} type="email" name="email" id="email" />
-                      <ErrorMessage component={FormError} name="email" />
-                    </FormControl>
-                    <FormControl>
-                      <Label htmlFor="subject">Subject</Label>
-                      <Field component={Input} type="text" name="subject" id="subject" />
-                      <ErrorMessage component={FormError} name="subject" />
-                    </FormControl>
-                    <FormControl>
-                      <Label htmlFor="body">Message</Label>
-                      <Field component={Textarea} type="number" name="body" id="body" />
-                      <ErrorMessage component={FormError} name="body" />
-                    </FormControl>
-                    <Button fullWidth type="submit" disabled={isSubmitting || !isValid}>
-                      Send message
-                    </Button>
-                  </Form>
-                )}
-              </Formik>
+      <div className="flex flex-col max-w-xl mx-auto flex-grow justify-center">
+        <Heading size="small">Get in touch</Heading>
+        <Text>
+          If you want to get in touch with me after all you've seen, then please fill this form and
+          I'll get back to you within a few hours. Alternatively, feel free to email me directly at{' '}
+          <a className="text-blue-600" href="mailto:contact@aggelosarvanitakis.me">
+            contact@aggelosarvanitakis.me
+          </a>
+          .
+        </Text>
+        {isMessageSent ? (
+          <div className="px-4 py-8 mt-4 rounded bg-black text-white text-center">
+            Your message has been sent!
+          </div>
+        ) : (
+          <Formik
+            initialValues={initialValues}
+            onSubmit={sendMessage}
+            validationSchema={validationSchema}
+          >
+            {({ isSubmitting, isValid }) => (
+              <FormikForm className="space-y-12" noValidate>
+                <fieldset>
+                  <label htmlFor="email" className="text-sm text-gray-500">
+                    Your email address
+                  </label>
+                  <Field component={Input} type="email" name="email" id="email" />
+                  <ErrorMessage component={FormError} name="email" />
+                </fieldset>
+                <fieldset>
+                  <label htmlFor="subject" className="text-sm text-gray-500">
+                    Subject
+                  </label>
+                  <Field component={Input} type="text" name="subject" id="subject" />
+                  <ErrorMessage component={FormError} name="subject" />
+                </fieldset>
+                <fieldset>
+                  <label htmlFor="body" className="text-sm text-gray-500">
+                    Message
+                  </label>
+                  <Field component={Textarea} type="number" name="body" id="body" />
+                  <ErrorMessage component={FormError} name="body" />
+                </fieldset>
+                <Button fullWidth type="submit" disabled={isSubmitting || !isValid}>
+                  Send message
+                </Button>
+              </FormikForm>
             )}
-          </Column>
-        </Grid>
-      </Container>
+          </Formik>
+        )}
+      </div>
     </Layout>
   );
 };
