adda0c75ad963e95888e688dc743f6d85a590ba7
3nvi <agg.arvanitakis@gmail.com>
2025-03-21 23:51:41 +0200

    refactor: remove unnecessary 'full' class from Button component for cleaner styling

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -6,7 +6,7 @@ const Button = ({ children, disabled, ...props }) => (
   <button
     disabled={disabled}
     className={clsx(
-      'w-full cursor-pointer rounded-3xl bg-gradient-to-l from-primary to-accent px-12 py-4 font-[system-ui] text-sm font-bold uppercase leading-none tracking-[0.15rem] text-white transition-all duration-150 ease-in-out hover:scale-[1.065] md:w-auto',
+      'w-full rounded-3xl bg-gradient-to-l from-primary to-accent px-12 py-4 font-[system-ui] text-sm font-bold uppercase leading-none tracking-[0.15rem] text-white transition-all duration-150 ease-in-out hover:scale-[1.065] md:w-auto',
       disabled && 'disabled:pointer-events-none disabled:opacity-30'
     )}
     {...props}
